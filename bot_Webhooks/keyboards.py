1da8a99be63129692df5192429e7507b29d6e3e4
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-11-22 22:13:00 -0500

    feat: agregar teclado principal con opciones de consultas del bot

---
+++ bot_Webhooks/keyboards.py
@@ -0,0 +1,9 @@
+keyboard_main = {
+    "keyboard": [
+        [{"text": "Horario de hoy"}],
+        [{"text": "Horario de mañana"}],
+        [{"text": "Mi semana"}],
+        [{"text": "Sugerencias IA"}]
+    ],
+    "resize_keyboard": True
+}
